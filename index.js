a780a7df038372f158d4dee9ab152606523872ae
oasosvaldo <oasosvaldo@gmail.com>
2024-03-19 12:42:00 -0300

    Finalizando Back And do CRUD

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const { cpuUsage } = require("process")
 
 server.use(express.json({extended: true}))
 
-const readFile = () => {
+const readFile = () => { // funcao readFile le e converte um arquivo JSON para texto
     const content = fs.readFileSync('./data/items.json', 'utf-8')
     return(JSON.parse(content))    
 }
 
-const writeFile = (content) => {
+const writeFile = (content) => { // funcao writeFile le e converte uma arquivo texto para JSON
     const updateFile = JSON.stringify(content)
     fs.writeFileSync('./data/items.json', updateFile, 'utf-8')
 }
 
-router.get('/', (req, res) => {
+router.get('/', (req, res) => { // le o arquivo JSON
     const content = readFile()
     res.send(content)
 })
 
-router.post('/', (req, res) => {
+router.post('/', (req, res) => { // Cria um dado dentro do arquivo JSON
     const {name, email, phone} = req.body
     const currentContent = readFile()
     const id = Math.random().toString(32).substr(2, 9)
@@ -29,16 +29,36 @@ router.post('/', (req, res) => {
     writeFile(currentContent)
     res.send({id, name, email, phone})
 })
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res) => { // Altera um dado dentro do ar
+    const {id} = req.params
+
     const {name, email, phone} = req.body
+    
+    const currentContent = readFile()
+    const serlectItem = currentContent.findIndex((item) => item.id === id)
+
+    const {id: cId, name:cName, email: cEmail, phone: cPhone} = currentContent[serlectItem]
+
+    const newObject = {
+        id: cId,
+        name: name ? name: cName,
+        email: email ? email: cEmail,
+        phone: phone ? phone: cPhone,
+    }
+
+    currentContent[serlectItem] = newObject
+    writeFile(currentContent)
+
+    res.send(newObject)
+})
+
+router.delete('/:id', (req, res) => {
     const {id} = req.params
     const currentContent = readFile()
     const serlectItem = currentContent.findIndex((item) => item.id === id)
-    currentContent[serlectItem] = {name, email, phone}
-    res.send(currentContent)
-})
-router.delete('/', (req, res) => {
-    res.send('Bem Vindo')
+    currentContent.splice(serlectItem, 1)
+    writeFile(currentContent)
+    res.send(true)
 })
 
 server.use(router)
